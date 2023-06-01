9f5384e66d026f9bc9ea18c0a482ea35aea2ec3a
Hiago Oliveira <hiagosantos223@gmail.com>
2023-06-01 19:47:43 -0300

    Entrega Aula01

---
+++ index.js
@@ -0,0 +1,6 @@
+let nome = prompt ("Insira seu nome");
+let sobrenome = prompt ("Insira seu sobrenome");
+
+alert ("Olá" + " " + nome + " " + sobrenome + " " + "!");
+
+console.log (`Olá ${nome} ${sobrenome}!`);
